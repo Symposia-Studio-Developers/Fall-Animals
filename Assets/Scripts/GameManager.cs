c4cddf080ea2ba10efe43374e74d1c78be457f73
IncentWang <wwhincent@gmail.com>
2023-06-15 12:17:54 -0700

    Initial Commit. - With Demo v0.1 ready inside.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace Fall_Friends.Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+        public Rigidbody Player1;
+        public Rigidbody Player2;
+        public Rigidbody Player3;
+
+        public Vector3 CenterPosition;
+        public float PowerCoefficient = 1.0f;
+
+        void Update()
+        {
+            float distanceFixedCoefficient = 1.0f;
+            if (Input.GetKey(KeyCode.F1))
+            {
+                Vector3 direction = CenterPosition - Player1.transform.position;
+                if (direction.magnitude > 0.5f)
+                {
+                    if (direction.magnitude < 3f)
+                    {
+                        distanceFixedCoefficient = 3.0f;
+                    }
+                    else
+                    {
+                        distanceFixedCoefficient = 1.0f;
+                    }
+                    // If not inside the center, try to get inside
+                    direction = direction.normalized;
+                    Player1.AddForce(direction * PowerCoefficient * distanceFixedCoefficient);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.F2))
+            {
+                Vector3 direction = CenterPosition - Player2.transform.position;
+                if (direction.magnitude > 0.5f)
+                {
+                    if (direction.magnitude < 3f)
+                    {
+                        distanceFixedCoefficient = 3.0f;
+                    }
+                    else
+                    {
+                        distanceFixedCoefficient = 1.0f;
+                    }
+                    // If not inside the center, try to get inside
+                    direction = direction.normalized;
+                    Player2.AddForce(direction * PowerCoefficient * distanceFixedCoefficient);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.F3))
+            {
+                Vector3 direction = CenterPosition - Player3.transform.position;
+                if (direction.magnitude > 0.5f)
+                {
+                    if (direction.magnitude < 3f)
+                    {
+                        distanceFixedCoefficient = 3.0f;
+                    }
+                    else
+                    {
+                        distanceFixedCoefficient = 1.0f;
+                    }
+                    // If not inside the center, try to get inside
+                    direction = direction.normalized;
+                    Player3.AddForce(direction * PowerCoefficient * distanceFixedCoefficient);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Stopper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Fall_Friends.Colliders
+{
+    public class Stopper : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                rb.mass = rb.mass * 1.0001f;
+                var trans = rb.transform;
+                trans.localScale = trans.localScale * 1.0001f;
+            }
+        }
+    }
+}
